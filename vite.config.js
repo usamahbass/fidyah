9f5e5bf6315d1882afed1972dd5e99fbf9d93b2e
usamahbass <basalamahusamah7@gmail.com>
2023-06-11 05:11:53 +0700

    fix: absolute import

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@fidyah": path.resolve(path.__dirname, "src"),
+      "@fidyah": path.resolve(__dirname, "src"),
     },
   },
 });
