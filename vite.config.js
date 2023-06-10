9b28403f1e901d2d9ff6a4abd9a1e0afce1ec250
usamahbass <basalamahusamah7@gmail.com>
2023-06-11 05:06:50 +0700

    add: dependencies and absolute import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@fidyah": path.resolve(path.__dirname, "src"),
+    },
+  },
+});
